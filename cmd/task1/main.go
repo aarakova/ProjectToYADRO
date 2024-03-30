de2c19e8afe6063c9bb63ec33a5bd295075cbf4e
aarakova <alyona.r2004@inbox.ru>
2024-03-30 21:16:37 +0300

    Исправлен main.go в соответствии с замечаниями

--- cmd/task1/main.go
+++ cmd/task1/main.go
@@ -1,48 +1,104 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
+	"time"
+	"unicode"
 
 	"github.com/kljensen/snowball"
 )
 
-var inputText string
+const TextLanguage = "english"
 
-var stopWords = "a an the and that but or as if when that because while " +
-	"where after so though since until whether before although nor not like " +
-	"once unless now except of on in " +
-	"i i'm i'll i'd i've you you're you'll you'd you've he he's he'll he'd " +
-	"she she's she'll she'd it it's we we're we'll we'd we've " +
-	"they they're they'll they'd they've me him her us them my your its our their " +
-	"am is are can have had could may might shall should will wount must "
+func NewFormatter(pathToFile string) (*Formatter, error) {
+	format := Formatter{stopWordsMap: make(map[string]bool)}
 
-func isStopWord(word string) bool {
-	return strings.Contains(stopWords, word)
+	var err error
+
+	if format.file, err = os.OpenFile(filepath.Clean(pathToFile), os.O_RDONLY, os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	if err = format.loadStopWords(); err != nil {
+		return nil, err
+	}
+
+	return &format, nil
 }
 
-func normalizeText(inputText string) string {
-	var result string
-	words := strings.Fields(inputText)
+type Formatter struct {
+	stopWordsMap map[string]bool
+
+	file *os.File
+}
+
+func (f *Formatter) loadStopWords() error {
+	scanner := bufio.NewScanner(f.file)
+	for scanner.Scan() {
+		f.stopWordsMap[scanner.Text()] = true
+	}
+
+	return scanner.Err()
+}
+
+func (f *Formatter) isStopWord(word string) bool {
+	return f.stopWordsMap[word]
+}
+
+func (f *Formatter) normalizeText(inputText string) []string {
+	var result []string
+	seenWords := make(map[string]bool)
+	words := strings.FieldsFunc(inputText, func(c rune) bool {
+		return unicode.IsSpace(c)
+	})
 	for _, word := range words {
-		stemmedWord, _ := snowball.Stem(word, "english", true)
-		if isStopWord(stemmedWord) || strings.Contains(result, stemmedWord) {
+		stemmedWord, _ := snowball.Stem(word, TextLanguage, true)
+		if f.isStopWord(stemmedWord) || seenWords[stemmedWord] {
 			continue
 		}
 
-		result += stemmedWord + " "
+		result = append(result, stemmedWord)
+		seenWords[stemmedWord] = true
 	}
 	return result
 }
 
-func main() {
-	flag.StringVar(&inputText,
-		"s",
+func NewNormalizerConfig() *NormalizerConfig {
+	var config NormalizerConfig
+	flag.StringVar(&config.inputText,
+		"src",
 		"",
 		"enter your string for analyzer",
 	)
+	flag.StringVar(&config.stopWordsFilePath,
+		"stop",
+		"stopWords.txt",
+		"file containing stop words",
+	)
 	flag.Parse()
 
-	log.Printf("%s", normalizeText(inputText))
+	return &config
+}
+
+type NormalizerConfig struct {
+	inputText         string
+	stopWordsFilePath string
+}
+
+func main() {
+	conf := NewNormalizerConfig()
+	formatter, err := NewFormatter(conf.stopWordsFilePath)
+	if err != nil {
+		log.Fatalf("cannot create fromatter: %s", err)
+	}
+	timer := time.Now()
+
+	log.Printf("Нормальизованный текст: \"%s\"", strings.Join(formatter.normalizeText(conf.inputText), " "))
+
+	log.Printf("Время выполнения программы: %fs", time.Since(timer).Seconds())
 }
